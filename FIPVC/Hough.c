282fa4c6260b64a2298014f445685a0898fa3944
leon <dream2rivendell@gmail.com>
2018-11-27 02:15:35 +0800

    save

--- FIPVC/Hough.c
+++ FIPVC/Hough.c
@@ -10,7 +10,7 @@
 #include "mathematic.h"
 
 #define THETA_RANGE 180 // -90 <= theta <= 90
-#define THETA_STEP 0.5 //控制theta 最小步长
+#define THETA_STEP 0.2 //控制theta 最小步长
 #define RHO_RANGE(width, height) sqrt(width * width + height * height)
 
 
@@ -25,7 +25,7 @@
  根据  p = x*cos(θ) + y * sin(θ)
  θ就是图像上直线相对于y轴的角度
  (θ, p) 表明 图像中满足：角度为θ, 长度为p的法线
- 的线段就是图像中的直线部分，发现和直线的交汇点可由 tan(θ) = x/y
+ 的线段就是图像中的直线部分，发现和直线的交汇点可由 tan(θ) = y/x
  得出
 
  @param image 绘制了直线霍夫直线的图像
@@ -36,16 +36,14 @@ void DrawLine(Matrix *image, double theta, double pho){
     int width = image->width;
     int height = image->height;
      for (int x = 0; x < width; x++) {
-         //tan(θ) θ == 90 和 -90 时无值
-         if(theta != - M_PI_2 || theta !=  M_PI_2){
-             int y = x/tan(theta);
-             if(theta > 0 && y <= height){
-                 image->array[y * width + x] = 255;
-             }else if(theta < 0 && y > height){
+         //sin(θ) θ == 0 为0
+         if(theta == 0){
+            
+         }else{
+             int y = (pho - x * cos(theta))/sin(theta) ;
+             if(y < height && y >= 0){
                  image->array[y * width + x] = 255;
              }
-         }else{
-             
          }
      }
     
@@ -74,12 +72,10 @@ void PolarShift(Matrix *polar, int x, int y){
     double theta = 0;
     int pho = 0;
     int half_polar_height = polar->height/2;
-    for (int j = 0; j < polar->height ; j++) {
-        for (int i = 0; i < polar->width; i++) {
+    for (int i = 0; i < polar->width; i++) {
             theta = (-90 + i * THETA_STEP) * M_PI/180;
             pho =  (int)(x * cos(theta) + y * sin(theta)) + half_polar_height;
-            polar->array[polar->width * pho + i]++;
-        }
+            polar->array[polar->width * pho + i] += 1;
     }
 }
 Matrix * Hough(Matrix *src, Matrix *dst, int length){
@@ -91,10 +87,21 @@ Matrix * Hough(Matrix *src, Matrix *dst, int length){
     for (int j = 0; j < height ; j++) {
         for (int i = 0; i < width; i++) {
             if(src->array[j * width + i] == 255)
-              PolarShift(polar, i, j);
+                PolarShift(polar, i, j);
         }
     }
     
+    int sum = 0;
+    for (int j = 0; j < polar->height ; j++) {
+        for (int i = 0; i < polar->width; i++) {
+            int index = j * polar->width + i;
+            int value = polar->array[index];
+            if( value >= length){
+                sum += 1;
+            }
+        }
+    }
+    printf("there are %d line over %d dot!\n",sum, length);
     for (int j = 0; j < polar->height ; j++) {
         for (int i = 0; i < polar->width; i++) {
             int index = j * polar->width + i;
@@ -102,10 +109,9 @@ Matrix * Hough(Matrix *src, Matrix *dst, int length){
                 double theta =  (-90 + i * THETA_STEP) * M_PI/180;
                 DrawLine(dst, theta, j - rho_height/2);
             }
-      
+
         }
     }
-    
     
     for(int i=0;i<polar->width * polar->height;i++){
         polar->array[i] = (polar->array[i] != 0 ? 255 : 0);
--- FIPVC/main.cpp
+++ FIPVC/main.cpp
@@ -40,25 +40,24 @@ char *Path(const char * fileName){
 }
 void doSomething(Matrix *src, Matrix *dst);
 
-int main(int argc, const char * argv[]) {
-    
 
-        int height =  101;
-        int width = 101;
-     Matrix * m_src =  matrixMake(width, height);
-     Matrix * m_dst =  matrixMake(width, height);
-        for (int j=0;j<height; j++) {
-            for(int i=0;i<width;i++){
-                int index = j * width + i;
-                if(index == 0 ||
-                   (j == 0 && i == width - 1) ||
-                   (j == height - 1 && i == width - 1) ||
-                   (j == height - 1 && i == 0) ||
-                   (j == height/2 && i == width/2 ))
+void houghLineTest(){
+    int height =  101;
+    int width = 101;
+    Matrix * m_src =  matrixMake(width, height);
+    Matrix * m_dst =  matrixMake(width, height);
+    for (int j=0;j<height; j++) {
+        for(int i=0;i<width;i++){
+            int index = j * width + i;
+            if(index == 0 ||
+               (j == 0 && i == width - 1) ||
+               (j == height - 1 && i == width - 1) ||
+               (j == height - 1 && i == 0) ||
+               (j == height/2 && i == width/2 ))
                 m_src->array[index] = 255;
-            }
         }
-    Matrix *houghMat  = Hough(m_src, m_dst, 0);
+    }
+    Matrix *houghMat  = Hough(m_src, m_dst, 3);
     IplImage *image =cvCreateImage(cvSize(houghMat->width, houghMat->height), 8, 1);
     for (int j=0;j<houghMat->height; j++) {
         for(int i=0;i<houghMat->width;i++){
@@ -68,74 +67,126 @@ int main(int argc, const char * argv[]) {
     cvNamedWindow("houghMat", 1);
     cvShowImage("houghMat", image);
     cvWaitKey(0);
+}
+
+
+void sbsb(){
     
-    //Fig1010(a)(five-dots).tif
-//    char *path = Path("nana.png");
-//    IplImage *src =cvLoadImage(path, 0);
-//    int height =  src->height;
-//    int width = src->width;
-//    Matrix * m_src = matrixMake(width, height);
-//    Matrix * m_dst = matrixMake(width, height);
-//    for (int j=0;j<height; j++) {
-//        for(int i=0;i<width;i++){
-//            m_src->array[j*width+i]=cvGetReal2D(src,j,i);
-//        }
-//    }
-//
-//    doSomething(m_src,m_dst);
-//
-//    IplImage *dst =cvCreateImage(cvSize(width, height), src->depth, 1);
-//    for (int j=0;j<height; j++) {
-//        for(int i=0;i<width;i++){
-//            cvSetReal2D(dst, j, i,m_dst->array[j*width+i]);
-//        }
-//    }
-//    cvNamedWindow("src", 1);
-//    cvShowImage("src", src);
-//
-//    cvNamedWindow("dst", 1);
-//    cvShowImage("dst", dst);
-//
-//
-//    IplImage *cv_dst =  cvCreateImage(CvSize(width,height), src->depth, 1);
-//    cvCanny(src, cv_dst, low_threshold, hight_threshold);
-//
-//    cvNamedWindow("opencv_dst", 1);
-//    cvShowImage("opencv_dst", cv_dst);
+    IplImage *srcRGB = cvLoadImage(Path("22.jpeg"));
+    IplImage *src = cvCreateImage(cvSize(srcRGB->width,srcRGB->height),IPL_DEPTH_8U,1);
+    
+    cvCvtColor(srcRGB,src,CV_RGB2GRAY);
+    IplImage *dst = cvCreateImage(cvSize(src->width,src->height),IPL_DEPTH_8U,src->nChannels);
+    IplImage *color_dst = cvCloneImage(srcRGB);cvSetZero(color_dst);
+    CvMemStorage *storage = cvCreateMemStorage();
+    CvSeq *lines = 0;
+    
+    
+    cvCanny(src,dst,40,90);
+    cvCvtColor(dst,color_dst,CV_GRAY2RGB);
+    
+    
+    
+    
+    
+#if 1
+    lines = cvHoughLines2(dst
+                          ,storage
+                          ,CV_HOUGH_STANDARD
+                          ,1
+                          ,CV_PI/180
+                          ,150
+                          ,0
+                          ,0);
+    for(int i = 0;i<MIN(lines->total,100);i++){
+        float *line = (float*)cvGetSeqElem(lines,i);
+        float rho = line[0];
+        float threta = line[1];
+        CvPoint pt1, pt2; // 这个地方的计算公式，在文章下面介绍。
+        double a = cos(threta),b = sin(threta);
+        double x0 = a*rho, y0 = b * rho;
+        pt1.x = cvRound(x0 + 1000*(-b));
+        pt1.y = cvRound(y0 + 1000*(a));
+        pt2.x = cvRound(x0 - 1000*(-b));
+        pt2.y = cvRound(y0 - 1000*(a));
+        cvLine(color_dst,pt1,pt2,CV_RGB(255,0,0),1,8);
+    }
+#else
+    lines = cvHoughLines2(dst,
+                          storage,
+                          CV_HOUGH_PROBABILISTIC,
+                          1,
+                          CV_PI/180,
+                          100,
+                          50,
+                          10);
+    for(int i = 0;i<lines->total; ++i){
+        CvPoint *line = (CvPoint*)cvGetSeqElem(lines,i);
+        cvLine(color_dst,line[0],line[1],CV_RGB(255,0,0),1,8);
+    }
+#endif
+    
+    
+    cvShowImage("source",src);
+    cvShowImage("Hough",color_dst);
+    cvShowImage("dst",dst);
     cvWaitKey(0);
     
-//    matrixFree(m_src);
-//    matrixFree(m_dst);
+
+}
+int main(int argc, const char * argv[]) {
+ 
+//    houghLineTest();
+    
+    char *path = Path("22.jpeg");
+    IplImage *src =cvLoadImage(path, 0);
+    int height =  src->height;
+    int width = src->width;
+    Matrix * m_src = matrixMake(width, height);
+    Matrix * m_dst = matrixMake(width, height);
+    for (int j=0;j<height; j++) {
+        for(int i=0;i<width;i++){
+            m_src->array[j*width+i]=cvGetReal2D(src,j,i);
+        }
+    }
+
+    doSomething(m_src,m_dst);
+
+
+    Matrix * Hough_dst = matrixMake(width, height);
+
+    m_dst  =  Hough(m_dst, Hough_dst, height * 0.8);
+
+    IplImage *dst =cvCreateImage(cvSize(m_dst->width, m_dst->height), src->depth, 1);
+    for (int j=0;j<m_dst->height; j++) {
+        for(int i=0;i<m_dst->width;i++){
+            cvSetReal2D(dst, j, i,m_dst->array[j*m_dst->width+i]);
+        }
+    }
+    cvNamedWindow("src", 1);
+    cvShowImage("src", src);
+
+    cvNamedWindow("dst", 1);
+    cvShowImage("dst", dst);
+
+
+    IplImage *cv_dst =  cvCreateImage(CvSize(width,height), src->depth, 1);
+    cvCanny(src, cv_dst, low_threshold, hight_threshold);
+
+    cvNamedWindow("opencv_dst", 1);
+    cvShowImage("opencv_dst", cv_dst);
+    cvWaitKey(0);
+
+    matrixFree(m_src);
+    matrixFree(m_dst);
     return 0;
 }
 
 
 void doSomething(Matrix *src, Matrix *dst){
-//    Matrix *gauss = matrixMake(5, 5);
-//    double arr[25] = { 2, 4, 5, 4, 2,
-//        4, 9,12, 9, 4,
-//        5,12,15,12, 5,
-//        4, 9,12, 9, 4,
-//        2, 4, 5, 4, 2};
-//    Matrix *filter = matrixMake(3, 3);
-//    double arr[25] = { -1, -1, -1, 2, 2,
-//        2, -1,-1, -1};
-//    matrixSet(filter, arr);
-//    matrixConvolution(src, dst, filter);
-//    matrixMultreal(dst, dst, 1.0/159.0);
-//    matrixFree(filter);
-    IplImage *src_image =cvLoadImage(Path("nana.png"), 0);
+  
     Canny(src, dst, 3, low_threshold, hight_threshold);
-    Matrix *houghMat  = Hough(dst, NULL, 0);
-    IplImage *image =cvCreateImage(cvSize(houghMat->width, houghMat->height), src_image->depth, 1);
-    for (int j=0;j<houghMat->height; j++) {
-        for(int i=0;i<houghMat->width;i++){
-            cvSetReal2D(image, j, i,houghMat->array[j * houghMat->width+i]);
-        }
-    }
-    cvNamedWindow("houghMat", 1);
-    cvShowImage("houghMat", image);
-    cvWaitKey(0);
+   
 }
 
 
